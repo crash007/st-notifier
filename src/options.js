849778625f5b5d88c02dfcd56343d481d4518443
Petter Wallin <wallin.petter@gmail.com>
2019-01-02 00:11:45 +0100

    Export chache to json-file

--- src/options.js
+++ src/options.js
@@ -5,40 +5,57 @@ function save_options() {
     console.log(notify);
 
     chrome.storage.local.set({
-      'intervall': intervall,
-      'notify': notify
-    }, function() {
+        'intervall': intervall,
+        'notify': notify
+    }, function () {
 
-        chrome.alarms.get("checkerAlarm", function(alarm){
+        chrome.alarms.get("checkerAlarm", function (alarm) {
             alarm.periodInMinutes = intervall;
             console.log(alarm);
         });
-      
 
-      var status = document.getElementById('status');
-      status.textContent = 'Sparat';
-      setTimeout(function() {
-        status.textContent = '';
-      }, 750);
+
+        var status = document.getElementById('status');
+        status.textContent = 'Sparat';
+        setTimeout(function () {
+            status.textContent = '';
+        }, 750);
     });
-  }
-  
-  // Restores select box and checkbox state using the preferences
-  // stored in chrome.storage.
-  function restore_options() {
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
     // Use default value color = 'red' and likesColor = true.
     chrome.storage.local.get({
         'intervall': true,
         'notify': 15
-    }, function(items) {
-      document.getElementById('intervall').value = items.intervall;
-      document.getElementById('notify').checked = items.notify;
+    }, function (items) {
+        document.getElementById('intervall').value = items.intervall;
+        document.getElementById('notify').checked = items.notify;
     });
 
-    chrome.storage.local.getBytesInUse(function(bytes){
+    chrome.storage.local.getBytesInUse(function (bytes) {
         console.log(bytes)
         document.getElementById('mem').textContent = bytes;
     });
-  }
-  document.addEventListener('DOMContentLoaded', restore_options);
-  document.getElementById('save').addEventListener('click',save_options);
+}
+
+function exportData() {
+    console.log("exporting data");
+    chrome.storage.local.get({ 'linksCache': {} }, function (result) {
+        var linksCache = JSON.stringify(result.linksCache);
+        console.log(linksCache);
+
+        // Save as file
+        var url = 'data:application/json;base64,' + btoa(unescape(encodeURIComponent(linksCache)));
+        chrome.downloads.download({
+            url: url,
+            filename: 'export.json'
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
+document.getElementById('export').addEventListener('click', exportData);
