07d2e78086aa1f4f3b802f6ecd0ebc434df21751
Petter Wallin <wallin.petter@gmail.com>
2019-01-07 01:09:50 +0100

    replace strings with variables

--- src/libs/compression.js
+++ src/libs/compression.js
@@ -2,7 +2,7 @@ function compress(content){
     
     //Firefox compatible
     var replaced = insertVariables(content);
-    
+    //console.log(replaced);
     var compressedContent = LZString.compressToUTF16(content);
     var compressedReplaced = LZString.compressToUTF16(replaced);
 
@@ -17,16 +17,43 @@ function decompress(compressed){
 }
 
 function insertVariables(content){
+   
+    var originalLength = content.length;
     
-    var replaced = content.replace(/https:\/\/bildix.mmcloud.se\/bildix\/api\/images\//gi, '$1$').replace(/.jpg\?fit=crop&amp;w=/g,'$2$');
-    var diff = content.length - replaced.length;
-    console.log("Inserting variables. length before was "+content.length+"  and length after "+replaced.length+ " , difference: "+diff);
-
-    return replaced;
+    $.each(getReplacementStrings(), function(index, value){
+        content = content.split(value).join('$'+index+'$');   
+    });
+    var diff = originalLength- content.length;
+    console.log("Inserting variables. length before was "+originalLength+"  and length after "+content.length+ " , difference: "+diff);
+    
+    return content;
 }
+
 
 function replaceVariables(content){
     console.log("replaceing variables");
-    return content.replace(/\$1\$/g,'https://bildix.mmcloud.se/bildix/api/images/').replace(/\$2\$/g, '.jpg?fit=crop&amp;w=')
+    var strings = getReplacementStrings();
+    $.each(strings, function(index, value){
+        content = content.split('$'+index+'$').join(value);
+    });
+    return content;
 }
 
+
+function getReplacementStrings(){
+
+    return ['https://bildix.mmcloud.se/bildix/api/images/', 
+        '.jpg?fit=crop&amp;w=',
+        '><figure class="inline-image-wrapper inline-resource"><div class="inline-inner-image-wrapper"><span class="m-icon-enlarge-image"><',
+        '1200 1200w" itemprop="image" class="lazy unobserved inline-image" alt="',
+        '" onerror="this.style.display=\'none !important\'"></div><div class="byline-caption"><div class="byline-caption-inner"><figcaption class="caption">',
+        '></div></div><div class="clearfix"></div></figure><p class="body">',
+        '</figcaption><div class="byline" itemprop="copyrightHolder">nBild: ',
+        '</p></section></div><div class="column large-two-thirds medium-full article-content"><section class="body" itemprop="articleBody"',
+        'div class="column large-full medium-full"><section class="leadin" itemprop="description"><p>',
+        '</a></section></div><div class="column large-third medium-third article-aside"></div></div>"',
+        '</p></section><section class="byline"><div class="name" itemprop="author">',
+        '<div class="row unpadded single-article"><',
+        '</p><p class="body">'
+    ];
+}
